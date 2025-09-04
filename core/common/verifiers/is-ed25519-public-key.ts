ed5b3b37ffeca364979d555f543e80d959472c2e
Fabricius Zatti <fazzatti@gmail.com>
2025-09-04 17:15:14 -0300

    fix(types): update isEd25519PublicKey to use type guard for Ed25519PublicKey

--- core/common/verifiers/is-ed25519-public-key.ts
+++ core/common/verifiers/is-ed25519-public-key.ts
@@ -1,5 +1,8 @@
 import { regex } from "../../mod.ts";
+import type { Ed25519PublicKey } from "../types.ts";
 
-export const isEd25519PublicKey = (value: string): boolean => {
-  return typeof value === "string" && regex.ed25519PublicKey.test(value);
+export const isEd25519PublicKey = (
+  address: string
+): address is Ed25519PublicKey => {
+  return typeof address === "string" && regex.ed25519PublicKey.test(address);
 };
