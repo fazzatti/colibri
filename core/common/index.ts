f2e271791a3b1a7c2848aef475c8c01d291afe5e
Fabricius Zatti <fazzatti@gmail.com>
2025-09-04 17:16:00 -0300

    feat: add verifiers export for transaction validation utilities

--- core/common/index.ts
+++ core/common/index.ts
@@ -1,1 +1,2 @@
 export { regex } from "../common/regex/index.ts";
+export * as verifiers from "./verifiers/index.ts";
---
+++ core/common/verifiers/index.ts
@@ -0,0 +1,4 @@
+export * from "./is-ed25519-public-key.ts";
+export * from "./is-fee-bump-transaction.ts";
+export * from "./is-muxed-address.ts";
+export * from "./is-transaction.ts";
