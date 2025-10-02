ee98414645d009fe9ddeab6dbb662405ecc66cb2
Fabricius Zatti <fazzatti@gmail.com>
2025-10-02 11:54:07 -0300

    feat: add assertRequiredArgs function to validate non-null arguments

---
+++ core/common/assert/assert-args.ts
@@ -0,0 +1,12 @@
+import type { ColibriError } from "../../mod.ts";
+
+// Asserts that all provided arguments are neither null nor undefined.
+// Throws the provided error if any argument is invalid.
+export function assertRequiredArgs(
+  args: Record<string, unknown>,
+  errorFn: (argName: string) => ColibriError
+): asserts args is Record<string, unknown> {
+  for (const argName of Object.keys(args)) {
+    if (!args[argName]) throw errorFn(argName);
+  }
+}
