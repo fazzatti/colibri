56140543a52724f859e512e858907b4e2fa64d28
Fabricius Zatti <fazzatti@gmail.com>
2025-10-20 08:27:33 -0300

    feat: add isTransactionSigner verifier function

--- core/common/verifiers/index.ts
+++ core/common/verifiers/index.ts
@@ -3,3 +3,4 @@ export * from "./is-muxed-id.ts";
 export * from "./is-signing-threshold.ts";
 export * from "./is-smart-contract-transaction.ts";
 export * from "./is-transaction.ts";
+export * from "./is-transaction-signer.ts";
---
+++ core/common/verifiers/is-transaction-signer.ts
@@ -0,0 +1,20 @@
+import type { TransactionSigner } from "../../signer/types.ts";
+
+export const isTransactionSigner = (
+  signer: unknown
+): signer is TransactionSigner => {
+  if (typeof signer !== "object" || signer === null) {
+    return false;
+  }
+
+  return (
+    "publicKey" in signer &&
+    typeof (signer as TransactionSigner).publicKey === "function" &&
+    "sign" in signer &&
+    typeof (signer as TransactionSigner).sign === "function" &&
+    "signTransaction" in signer &&
+    typeof (signer as TransactionSigner).signTransaction === "function" &&
+    "signSorobanAuthEntry" in signer &&
+    typeof (signer as TransactionSigner).signSorobanAuthEntry === "function"
+  );
+};
