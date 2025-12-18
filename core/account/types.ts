f8a5f1ab0b36f89acfe5b646d5ad25ec1a608e2f
Fabricius Zatti <fazzatti@gmail.com>
2025-12-18 10:09:10 -0300

    standardize account interfaces

--- core/account/types.ts
+++ core/account/types.ts
@@ -1,7 +1,27 @@
+import type { Asset, xdr } from "stellar-sdk";
 import type {
   Ed25519PublicKey,
   MuxedAddress,
   ContractId,
 } from "@/strkeys/types.ts";
+import type { MultiSigSchema, Signer } from "@/signer/types.ts";
 
-export type AnyAddress = Ed25519PublicKey | MuxedAddress | ContractId;
+export type StellarAddress = Ed25519PublicKey | MuxedAddress | ContractId;
+
+export interface Account {
+  address(): StellarAddress;
+  getAccountLedgerKey(): xdr.LedgerKey;
+  getTrustlineLedgerKey(asset: Asset): xdr.LedgerKey;
+}
+
+export type WithoutSigner<T extends Account> = T & {
+  withMasterSigner(signer: Signer): WithSigner<T>;
+};
+
+export type WithSigner<T> = T & {
+  signer(): Signer;
+};
+
+export type WithMultiSig<T> = T & {
+  getMultiSigSchema(): MultiSigSchema;
+};
