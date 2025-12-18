0c680eafb9c2da7a411318ff586480ba53c0913b
Fabricius Zatti <fazzatti@gmail.com>
2025-12-18 10:09:21 -0300

    add UNSUPPORTED_ADDRESS_TYPE error and update NativeAccount to handle unsupported address types

--- core/account/native/index.ts
+++ core/account/native/index.ts
@@ -5,28 +5,38 @@ import { isMuxedId } from "@/common/type-guards/is-muxed-id.ts";
 import type { Ed25519PublicKey, MuxedAddress } from "@/strkeys/types.ts";
 import type { Signer } from "@/signer/types.ts";
 import * as E from "@/account/native/error.ts";
+import type { INativeAccount, MuxedId } from "@/account/native/types.ts";
 import type {
+  StellarAddress,
   WithSigner,
-  NativeAccountType,
-  MuxedId,
   WithoutSigner,
-} from "@/account/native/types.ts";
+} from "@/account/types.ts";
 
-export class NativeAccount implements NativeAccountType {
-  protected _address: Ed25519PublicKey;
+export class NativeAccount implements INativeAccount {
+  protected _publicKey: Ed25519PublicKey;
   protected _masterSigner?: Signer;
 
-  private constructor(address: Ed25519PublicKey) {
+  private constructor(publicKey: Ed25519PublicKey) {
     assert(
-      StrKey.isValidEd25519PublicKey(address),
-      new E.INVALID_ED25519_PUBLIC_KEY(address)
+      StrKey.isValidEd25519PublicKey(publicKey),
+      new E.INVALID_ED25519_PUBLIC_KEY(publicKey)
     );
 
-    this._address = address;
+    this._publicKey = publicKey;
   }
 
-  static fromAddress(address: Ed25519PublicKey) {
-    return new NativeAccount(address) as WithoutSigner<NativeAccountType>;
+  static fromAddress(address: StellarAddress): WithoutSigner<NativeAccount> {
+    assert(
+      StrKey.isEd25519PublicKey(address),
+      new E.UNSUPPORTED_ADDRESS_TYPE(address)
+    );
+    return new NativeAccount(address) as WithoutSigner<NativeAccount>;
+  }
+
+  static fromPublicKey(
+    address: Ed25519PublicKey
+  ): WithoutSigner<NativeAccount> {
+    return new NativeAccount(address) as WithoutSigner<NativeAccount>;
   }
 
   // ----------------
@@ -34,19 +44,19 @@ export class NativeAccount implements NativeAccountType {
   // ----------------
 
   address(): Ed25519PublicKey {
-    return this._address;
+    return this._publicKey;
   }
 
   muxedAddress(id: MuxedId): MuxedAddress {
     assert(isMuxedId(id), new E.INVALID_MUXED_ID(id));
 
-    const baseAcc = new Account(this._address, "1");
+    const baseAcc = new Account(this._publicKey, "1");
     const acc = new MuxedAccount(baseAcc, id);
     const muxedAddress = acc.accountId();
 
     assert(
       StrKey.isValidMuxedAddress(muxedAddress),
-      new E.INVALID_MUXED_ADDRESS_GENERATED(muxedAddress, id, this._address)
+      new E.INVALID_MUXED_ADDRESS_GENERATED(muxedAddress, id, this._publicKey)
     );
 
     return muxedAddress as MuxedAddress;
@@ -55,7 +65,7 @@ export class NativeAccount implements NativeAccountType {
   getAccountLedgerKey(): xdr.LedgerKey {
     const ledgerKey = xdr.LedgerKey.account(
       new xdr.LedgerKeyAccount({
-        accountId: Keypair.fromPublicKey(this._address).xdrPublicKey(),
+        accountId: Keypair.fromPublicKey(this._publicKey).xdrPublicKey(),
       })
     );
 
@@ -65,7 +75,7 @@ export class NativeAccount implements NativeAccountType {
   getTrustlineLedgerKey(asset: Asset): xdr.LedgerKey {
     const trustlineLedgerKey = xdr.LedgerKey.trustline(
       new xdr.LedgerKeyTrustLine({
-        accountId: Keypair.fromPublicKey(this._address).xdrAccountId(),
+        accountId: Keypair.fromPublicKey(this._publicKey).xdrAccountId(),
         asset: asset.toTrustLineXDRObject(),
       })
     );
@@ -89,7 +99,7 @@ export class NativeAccount implements NativeAccountType {
   }
 
   signer(): Signer {
-    assert(this._masterSigner, new E.MISSING_MASTER_SIGNER(this._address));
+    assert(this._masterSigner, new E.MISSING_MASTER_SIGNER(this._publicKey));
     return this._masterSigner;
   }
 }
