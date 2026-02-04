306186bbc79e464dafbac4658f3ff82d1fa485ac
Fabricius Zatti <fazzatti@gmail.com>
2026-02-04 09:25:06 -0300

    feat: add environment loading utilities and update deno.json for improved testing

---
+++ _internal/env/index.ts
@@ -0,0 +1,3 @@
+import { loadOptionalEnv } from "./loadEnv.ts";
+
+export const QUASAR_API_KEY = loadOptionalEnv("QUASAR_API_KEY");
---
+++ _internal/env/loadEnv.ts
@@ -0,0 +1,30 @@
+import { load } from "@std/dotenv";
+
+const env = await load(); // Reads from .env file
+
+export const requireEnv = (key: string): string => {
+  const value = env[key as keyof typeof env];
+
+  if (value === undefined) {
+    const denoVal = Deno.env.get(key);
+
+    if (denoVal === undefined) {
+      throw new Error(`${key} is not loaded`);
+    }
+    return denoVal;
+  }
+  return value;
+};
+
+export const loadOptionalEnv = (key: string): string | undefined => {
+  const value = env[key as keyof typeof env];
+
+  if (value === undefined) {
+    const denoVal = Deno.env.get(key);
+    if (denoVal === undefined) {
+      return undefined;
+    }
+    return denoVal;
+  }
+  return value;
+};
